84acad4ea36357e19706ea9cfbab1ef17c245fe9
Sergio David Munoz <sdmunozsierra@miners.utep.edu>
2021-05-23 23:27:08 -0400

    Added test framework mocha

--- inbox/README.md
+++ inbox/README.md
@@ -4,4 +4,5 @@
 npm init
 npm install --save solc@0.4.17
 node compile.js
+npm install --save ganache-cli mocha web3@1.0.0-beta.26
 ```
--- inbox/compile.js
+++ inbox/compile.js
@@ -5,4 +5,7 @@ const solc = require('solc')
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8')
 
-console.log(solc.compile(source, 1)) // Number  of contracts to be compiled
+// console.log(solc.compile(source, 1)) // Number  of contracts to be compiled
+
+// returns a compiled contract
+module.exports = solc.compile(source, 1).contracts[':Inbox']
