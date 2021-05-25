aa9fbc9ee745ab49b176abbe64d1a66e91499336
Sergio David Munoz <sdmunozsierra@miners.utep.edu>
2021-05-25 00:15:33 -0400

    Added lottery boilerplate

--- inbox/compile.js
+++ inbox/compile.js
@@ -4,8 +4,11 @@ const solc = require('solc')
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8')
+const ballotPath = path.resolve(__dirname, 'contracts', 'Ballot.sol')
+const ballotSource = fs.readFileSync(ballotPath, 'utf8')
 
 // console.log(solc.compile(source, 1)) // Number  of contracts to be compiled
 
 // returns a compiled contract
-module.exports = solc.compile(source, 1).contracts[':Inbox']
+// module.exports = solc.compile(source, 1).contracts[':Inbox']
+module.exports = solc.compile(source, 1).contracts[':Ballot']
---
+++ inbox/contracts/Ballot.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.17
+
+contract Lottery {
+    address public manager;
+
+    function Lottery() public {
+        manager = msg.sender
+    }
+
+}
