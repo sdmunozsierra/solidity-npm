4f37ee02328f3744a156230ca4d664e05a051f56
Sergio David Munoz <sdmunozsierra@miners.utep.edu>
2021-06-10 22:14:24 -0400

    Added campaign factory contract

--- campaign/contracts/Ballot.sol
+++ campaign/contracts/Ballot.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.4.17;
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        // minimum contribution to participate
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+    
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+
+}
+
 /**
 @title Campaign
 @dev Implements voting process for money expenditure
@@ -10,53 +25,65 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;  // Type struct Request
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
     
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    function Campaign(uint minimum) public {
-        manager = msg.sender;
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
     
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(string description,uint  value, address recipient) public restricted {
+        // Use mapping to determine if the sender is approved
+        require(approvers[msg.sender]);
         // Create a struct and add it to the request array
         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
+            // The mapping (reference type) is not needed to be initialized
         });
         requests.push(newRequest);
     }
     
-    // function random() private view returns (uint) {
-    //     return uint(keccak256(block.difficulty, now, players));
-    // }
-    
-    // Uses restricted to allow only manager
-    // function pickWinner() public restricted {
-    //     uint index = random() % players.length;
-    //     players[index].transfer(this.balance);
-    //     lastWinner = players[index]; // Get last winner
-    //     players = new address[](0);  // clear players array
-    // }
-    
-    
-    // function getPlayers() public view returns (address[]) {
-    //     return players;
-    // }
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index]; // look for request in storage
+
+        require(request.approvalCount > (approversCount /2 )); // more than half of entered
+        require(!request.complete); // if not complete
+
+        request.recipient.transfer(request.value);
+        request.complete = true; // update complete
+    }
+
 }   
