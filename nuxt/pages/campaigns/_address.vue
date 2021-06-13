dd08cdb7abc30542c0888a4331c841beefa92bed
Sergio David Munoz <sdmunozsierra@miners.utep.edu>
2021-06-12 21:22:48 -0400

    Added dynamic routing

---
+++ nuxt/pages/campaigns/_address.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title> {{ address }}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      address: this.$route.params.address,
+    }
+  },
+}
+</script>
+
+<style></style>
